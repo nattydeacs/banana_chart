f1991b769d9412db7c0437db52af997239476124
nattydeacs <natdeacon@gmail.com>
2022-07-15 11:44:55 -0400

    minor updates

--- chart_script.R
+++ chart_script.R
@@ -6,7 +6,7 @@ df <- read.csv("FAOSTAT_data_7-7-2022.csv")
 sort(unique(df$Partner.Countries))
 
 nations <- c("Ecuador", "Philippines", "Costa Rica", "Colombia", "Guatemala", "Luxembourg", 
-             "Honduras", "Unites States of America", "United Arab Emerates", "Panama", "Cameroon")
+             "Honduras", "United States of America", "Panama", "Cameroon")
 
 df <- filter(df, Reporter.Countries %in% nations) %>%
   filter(Element == "Export Quantity")%>%
@@ -14,14 +14,22 @@ df <- filter(df, Reporter.Countries %in% nations) %>%
   summarise(tonnes_exports = sum(Value, na.rm = TRUE))
 
 banancols <- c("#7c9800", "#dd9821", "#804a2d", "#e4cf9e", "#f0eacf",
-              "#dbc900", "#ae6c38", "#acc600", "#938413", "#d48f1a")
+              "#dbc900", "#ae6c38", "#acc600", "#fee528", "#d48f1a")
 
 chart <- ggplot(df, aes(x= Year, y = tonnes_exports, fill = Reporter.Countries)) +
   geom_stream(type = "ridge") +
-  guides(fill = guide_legend(title = "Exporting Nation")) +
+  guides(fill = guide_legend(title = "Land")) +
   scale_fill_manual(values = banancols) +
-  scale_y_continuous(labels = label_number(suffix = " M", scale = 1e-6, accuracy = 1)) +
-  
+  scale_y_continuous(labels = label_number(suffix = " M", scale = 1e-6, accuracy = 2)) +
+  scale_x_continuous(breaks = seq(1986, 2020, 2)) +
+  xlab("")+
+  ylab("Export Von Banaanen (tonnes)")+
+  theme(legend.position = "right", panel.grid = element_blank(), 
+        panel.background = element_rect("#fcf9f3"),
+        plot.background = element_rect(fill = "#fcf9f3"),
+        legend.background = element_rect(fill = "#fcf9f3")) +
+  coord_cartesian(xlim = c(1987.5, 2018.75))+
+  ggtitle("Jährliche Bananenexporte, ausgewählte Länder")
 
 
 chart
